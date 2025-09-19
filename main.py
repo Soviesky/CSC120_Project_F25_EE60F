53b6e3cbacbdd841daf74fa4f7e5d8fbb18d9d46
Soviesky <dovibas8@gmail.com>
2025-09-19 11:32:53 -0400

    main.py

---
+++ main.py
@@ -0,0 +1,8 @@
+phoenix_game = "Phoenix"
+print("Welcome to",phoenix_game,"!")
+print("============================")
+print("Before we begin, what's your characters name?")
+name = input()
+
+print(">",name)
+print("Great,",name,"!","let's begin the adventure!")
