0572bd01fb0b2ae57cae42456a03cf7d7c114eb4
Soviesky <dovibas8@gmail.com>
2025-10-20 15:18:14 -0400

    added part 2

--- main.py
+++ main.py
@@ -1,8 +1,24 @@
 phoenix_game = "Phoenix"
-print("Welcome to",phoenix_game,"!")
+print(f"Welcome to",phoenix_game,"!")
 print("============================")
 print("Before we begin, what's your characters name?")
 name = input()
-
 print(">",name)
-print("Great,",name,"!","let's begin the adventure!")
+print(f"Great,",name,"!","let's begin the adventure!")
+player = {
+    "name" : name,
+    "health" : 100,
+    "coin" : 0
+}
+import random
+events = ["find a coin","meet a monster","do nothing"]
+event = random.choice(events)
+print(f"While exploring, you {event}!")
+
+if event == "find a coin":
+    player["coin"] =  1
+    print(name,"found a coin",name,"now has",player["coin"],"number of coins")
+
+elif event == "meet a monster":
+    player["health"] = 90
+    print(name,"got hurt during the combat with the monster, health is now",player ["health"])
